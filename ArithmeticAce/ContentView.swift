c7da5cc379016cdb2f00c3fe93e167881cbce955
lcs-ashin <ashin@lcs.on.ca>
2022-02-08 09:14:24 -0500

    Added a multiplication symbol and two numbers using HStack and VStack

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image(systemName: "multiply")
+            
+            Spacer()
+            
+            VStack {
+                Text("5")
+                Text("6")
+            }
+        }
+        .font(.system(size: 70).bold())
+        .padding(30)
     }
 }
 
