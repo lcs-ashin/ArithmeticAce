c858fc39e2ab0f5ed6e49de811aa82be36d97b02
lcs-ashin <ashin@lcs.on.ca>
2022-02-08 09:30:54 -0500

    Got the product and checkmark below a divider

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image(systemName: "multiply")
-            
-            Spacer()
-            
-            VStack {
-                Text("5")
-                Text("6")
+        VStack {
+            HStack {
+                Image(systemName: "multiply")
+                
+                Spacer()
+                
+                VStack {
+                    Text("5")
+                    Text("6")
+                }
             }
+            .font(.system(size: 70).bold())
+            .padding(30)
+            
+            Divider()
+            
+            HStack {
+                Image(systemName: "checkmark.circle")
+                
+                Spacer()
+                
+                Text("30")
+            }
+            .font(.system(size: 70).bold())
+            .padding(30)
         }
-        .font(.system(size: 70).bold())
-        .padding(30)
     }
 }
 
